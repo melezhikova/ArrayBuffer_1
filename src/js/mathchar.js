5c1e67126046acac2ff26abdb94af31f54191b91
Liudmila Melezhikova <melezhikova@yandex.ru>
2021-08-26 22:00:13 +0500

    second commit

--- src/js/mathchar.js
+++ src/js/mathchar.js
@@ -1,36 +1,29 @@
 import Character from './basic';
 
 export default class MathCharacter extends Character {
-    constructor(name, type) {
-        super(name, type);
+  set stoned(state) {
+    this.setStoned = state;
+  }
+
+  get stoned() {
+    return this.setStoned;
+  }
+
+  set attackPower(cells) {
+    this.cellsAttack = cells;
+  }
+
+  get attackPower() {
+    this.baseAttack = this.attack - this.attack * ((this.cellsAttack - 1) / 10);
+    if (this.baseAttack > 0 && !this.stoned) {
+      return this.baseAttack;
+    } if (this.baseAttack <= 0) {
+      return 0;
     }
-  
-    set stoned (state) {
-      this.setStoned = state;
+    this.totalAttack = this.baseAttack - Math.trunc(Math.log2(this.cellsAttack) * 5);
+    if (this.totalAttack <= 0) {
+      return 0;
     }
-  
-    get stoned () {
-      return this.setStoned;
-    }
-  
-    set attackPower (cells) {
-        this.cellsAttack = cells;
-    }
-    
-    get attackPower () {
-        this.baseAttack = this.attack - this.attack * (this.cellsAttack - 1) / 10;
-        if (this.baseAttack > 0 && !this.stoned) {
-            return this.baseAttack;
-        } else if (this.baseAttack <= 0) {
-            return 0;
-        } else {
-          this.totalAttack = this.baseAttack - Math.trunc(Math.log2(this.cellsAttack) * 5);
-          if (this.totalAttack <= 0) {
-              return 0;
-          } else {
-              return this.totalAttack;
-          }
-        }
-    }
-  
+    return this.totalAttack;
+  }
 }
